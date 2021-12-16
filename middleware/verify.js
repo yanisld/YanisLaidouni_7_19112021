bff73cff9f9151d6b76944e2ace89d2799d3885d
Yanis <yanis.l69520@live.fr>
2021-12-16 15:47:41 +0100

    Création des cookies/affichage post sur l'accueil

--- middleware/verify.js
+++ middleware/verify.js
@@ -2,6 +2,7 @@ const jwt = require('jsonwebtoken');
 
 module.exports.verifyUser = (req, res, next) => {
     try {
+        req.headers.authorization = req.cookies.access_token;
         const token = req.headers.authorization.split(' ')[1];
         const decodedToken = jwt.verify(token, 'secretKey');
         const userId = decodedToken.id;
--- public/js/home.js
+++ public/js/home.js
@@ -4,12 +4,28 @@ getAllPosts = async () => {
             const url = 'posts/';
             const result = await fetch(url);
             if (result.ok) {
-                const data = await result.json();
-                console.log(data);
+                const posts = await result.json();
+                displayAllPosts(posts);
             }
         }
         catch (err) {
             console.error(err);
         }
+}
+displayAllPosts = (posts) => {
+    const main = document.querySelector('#main');
+    for (let i in posts) {
+        const div = document.createElement("div");
+        const title = document.createElement("h2");
+        const content = document.createElement("div");
+
+        div.classList.add('post');
+        title.textContent = posts[i].title;
+        content.textContent = posts[i].content;
+
+        main.appendChild(div);
+        div.appendChild(title);
+        div.appendChild(content);
+    }
 }
 getAllPosts();
